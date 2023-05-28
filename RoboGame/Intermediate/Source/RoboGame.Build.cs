240a7a17a321606b1566d829d758d4f079df0281
HBB <maxdo1579@mail.ru>
2023-05-28 16:40:53 +0300

    adding simple sound system. remove all debug messages

---
+++ RoboGame/Intermediate/Source/RoboGame.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class RoboGame : ModuleRules
+{
+	public RoboGame(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
